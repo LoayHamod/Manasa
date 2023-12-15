4edef0d0a68a9c7b56657665506cd9c12dc6f7d5
Loay Hamod <112626695+LoayHamod@users.noreply.github.com>
2023-12-15 21:48:24 +0300

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors);
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+(async ()=> {
+  try {
+    await db.mongoose.connect(db.url);
+    console.log("Successfully connect to MongoDB.");
+  } catch (err) {
+    console.error("Connection error", err);
+    process.exit(1); // Exit with an error code
+  }
+})();
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to AOL application." });
+});
+
+require("./app/routes/lecture.routes")(app);
+require("./app/routes/exam.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
